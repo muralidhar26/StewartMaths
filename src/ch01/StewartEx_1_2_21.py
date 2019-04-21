5a8837f9241a0bd4c4b7823909dc78b2e642ae67
muralidhar26 <muralidhar26@gmail.com>
2019-04-20 23:46:55 -0700

    Initial Commit for Python module - Test

--- src/ch01/StewartEx_1_2_21.py
+++ src/ch01/StewartEx_1_2_21.py
@@ -43,7 +43,6 @@ Y_pred_200000 = m*200000 + c
 print ('Y_pred_200000-->'+ str(Y_pred_200000))
 
 plt.scatter(serX, serY) # actual
-# plt.scatter(X, Y_pred, color='red')
 plt.plot([max(serX), min(serX)], [min(Y_pred), max(Y_pred)], color='red') # predicted
 plt.show()
 
