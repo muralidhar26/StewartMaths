b8cef73353ea307e068516268183ed093ea998a1
muralidhar26 <muralidhar26@gmail.com>
2019-04-28 19:39:03 -0700

    Test change

--- src/ch01/StewartEx_1_3_14.py
+++ src/ch01/StewartEx_1_3_14.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# plt.plot([-2, 2], [-2, 2], 'ro')
+
 f = lambda x: x**2 + 0.02*np.sin(50*x)
 x = np.linspace(-10*np.pi, 10*np.pi,1000)
 print(x)
