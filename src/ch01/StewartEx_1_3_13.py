bc3f131cc5ac8998cf94102fae77f446b136471d
muralidhar26 <muralidhar26@gmail.com>
2019-04-28 19:44:54 -0700

    change in the input parameters

--- src/ch01/StewartEx_1_3_13.py
+++ src/ch01/StewartEx_1_3_13.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 # plt.plot([-2, 2], [-2, 2], 'ro')
 f = lambda x: 10*np.sin(x) + np.sin(100*x)
-x = np.linspace(-2*np.pi, 2*np.pi,100)
+x = np.linspace(-np.pi/25, np.pi/25,100)
 print(x)
 y = [f(i) for i in x]
-plt.axis([-2*np.pi, 2*np.pi, -14, 14])
+plt.axis([-np.pi/25, np.pi/25, -1, 1])
 plt.plot(x, y)
 plt.show()
